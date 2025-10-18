5c1c824d10317bf20f0ebe9375b41a374d779ea9
Rijan-Budds <buddsrijan@gmail.com>
2025-10-18 22:41:22 +0545

    first commit

---
+++ src/app/components/Avatar.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+
+function Avatar() {
+  return (
+    <Image className="rounded-full" src="/images/avatar.jpg" alt="User Avatar" width={30} height={30} />
+  )
+}
+
+export default Avatar
