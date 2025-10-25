0dfdc3248125207a0d7b1569881dba38e3958284
Rijan-Budds <buddsrijan@gmail.com>
2025-10-25 12:36:48 +0545

    formatting code

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -1,11 +1,17 @@
-'use client'
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import React from "react";
+import Image from "next/image";
 
 function Avatar() {
   return (
-    <Image className="rounded-full" src="/images/avatar.jpg" alt="User Avatar" width={30} height={30} />
-  )
+    <Image
+      className="rounded-full"
+      src="/images/avatar.jpg"
+      alt="User Avatar"
+      width={30}
+      height={30}
+    />
+  );
 }
 
-export default Avatar
+export default Avatar;
